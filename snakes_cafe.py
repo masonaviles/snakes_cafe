c738d623513800bb59e357fa1ca4f22ec7f79327
Mason Aviles <mce.aviles@gmail.com>
2021-05-17 14:21:01 -0500

    start on lab 1

---
+++ snakes_cafe.py
@@ -0,0 +1,84 @@
+# **************************************
+# **    Welcome to the Snakes Cafe!   **
+# **    Please see our menu below.    **
+# **
+# ** To quit at any time, type "quit" **
+# **************************************
+
+print("*" * 38)
+print("""**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **""")
+print("*" * 38)
+
+# Appetizers
+# ----------
+# Wings
+# Cookies
+# Spring Rolls
+
+print(""" """)
+
+print("""Appetizers
+----------
+Wings
+Cookies
+Spring Rolls""")
+
+# Entrees
+# -------
+# Salmon
+# Steak
+# Meat Tornado
+# A Literal Garden
+
+print(""" """)
+
+print("""Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden""")
+
+# Desserts
+# --------
+# Ice Cream
+# Cake
+# Pie
+
+print(""" """)
+
+print("""Desserts
+--------
+Ice Cream
+Cake
+Pie""")
+
+# Drinks
+# ------
+# Coffee
+# Tea
+# Unicorn Tears
+
+print(""" """)
+
+print("""Drinks
+------
+Coffee
+Tea
+Unicorn Tears""")
+
+# ***********************************
+# ** What would you like to order? **
+# ***********************************
+# >
+
+print(""" """)
+
+print("""***********************************
+** What would you like to order? **
+***********************************""")
+
+order = input("> ")
